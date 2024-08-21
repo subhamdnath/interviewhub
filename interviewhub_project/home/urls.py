86367f841cea8a45eddf9659d79a09770c1faf13
subhamdnath6 <subhamdnath6@gmail.com>
2024-08-21 22:26:05 +0530

    Added register and login template

--- interviewhub_project/home/urls.py
+++ interviewhub_project/home/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from home.views import *
 
 urlpatterns = [
-    path('', home_view, name="home_view" ),
+    path('', home_view, name="home" ),
+    path('register/', register_view, name="register" ),
+    path('login/', login_view, name="login" ),
     path('job/', job_view, name="job" ),
+
 
 ]
--- interviewhub_project/home/views.py
+++ interviewhub_project/home/views.py
@@ -4,6 +4,12 @@ from django.http import HttpResponse
 def home_view(request):
     return render(request, "home/home_view.html")
 
+def register_view(request):
+    return render(request, "home/register_view.html")
+
+def login_view(request):
+    return render(request, "home/login_view.html")
+
 def job_view(request):
     return render(request, "home/job.html")
 
